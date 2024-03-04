7c73ec88e5c585833a97cd2b0aca58b36ce5d592
natsuki.iwano <natsuki.iwano@ariseanalytics.com>
2024-03-04 12:42:40 +0900

    added script

---
+++ update_docs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# Activate your environment, if necessary
+# source /path/to/your/venv/bin/activate
+
+# Run your Python script
+python run.py
+
+# Navigate to the docs directory
+cd docs
+
+# Check for changes
+git add .
+
+# If there are changes, commit and push
+if [[ `git status --porcelain` ]]; then
+  git commit -m "Update docs"
+  git push origin main
+else
+  echo "No changes to commit."
+fi
