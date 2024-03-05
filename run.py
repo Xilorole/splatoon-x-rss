9d0a7ac4b7e610397e7e940fac9e0115b329db0f
natsuki.iwano <natsuki.iwano@ariseanalytics.com>
2024-03-05 09:38:09 +0900

    updated log

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import re
 import xml.etree.ElementTree as ET
@@ -15,6 +16,20 @@ from tqdm import tqdm
 
 load_dotenv()
 logger = getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
 
 
 class RSSFeed:
